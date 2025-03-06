68ec4c32c39e0a6cc02808e1c319775e88f4e2ca
Rishi Thakur <rishithakur9131@gmail.com>
2025-03-06 12:30:11 +0530

    [Rishi] Add. Ability to ensure Valid Contacts are added

--- AddressBook.js
+++ AddressBook.js
@@ -1,43 +1,56 @@
-// // Creating Class with name ContactDetails to store address-related details
-// class ContactDetails {
-//     constructor(firstName, lastName, address, city, state, zipCode, phoneNumber, email) {
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.address = address;
-//         this.city = city;
-//         this.state = state;
-//         this.zipCode = zipCode;
-//         this.phoneNumber = phoneNumber;
-//         this.email = email;
-//     }
-// }
 
-// // Creating an instance of ContactDetails
-// let contact = new ContactDetails(
-//     "Deepak",
-//     "Singh",
-//     "House No. 45",
-//     "Bhopal",
-//     "Madhya Pradesh",
-//     "462046",
-//     "9691876648",
-//     "singh017deepak@gmail.com"
-// );
+class Contact {
+    constructor(firstName, lastName, address, city, state, zip, phone, email) {
+        if (!this.isValidName(firstName)) throw new Error("Invalid First Name");
+        if (!this.isValidName(lastName)) throw new Error("Invalid Last Name");
+        if (!this.isValidAddress(address)) throw new Error("Invalid Address");
+        if (!this.isValidAddress(city)) throw new Error("Invalid City");
+        if (!this.isValidAddress(state)) throw new Error("Invalid State");
+        if (!this.isValidZip(zip)) throw new Error("Invalid Zip Code");
+        if (!this.isValidPhone(phone)) throw new Error("Invalid Phone Number");
+        if (!this.isValidEmail(email)) throw new Error("Invalid Email");
 
-// // Printing the contact details
-// console.log(contact); 
-
-class AddressClass {
-    constructor(first, last_names, address, city, state, zip, phone_number, email) {
-        this.first = first;
-        this.last_names = last_names;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.address = address;
         this.city = city;
         this.state = state;
         this.zip = zip;
-        this.phone_number = phone_number;
+        this.phone = phone;
         this.email = email;
     }
+
+    isValidName(name) {
+        return /^[A-Z][a-zA-Z]{2,}$/.test(name);
+    }
+
+    isValidAddress(value) {
+        return value.length >= 4;
+    }
+
+    isValidZip(zip) {
+        return /^[0-9]{5,6}$/.test(zip);
+    }
+
+    isValidPhone(phone) {
+        return /^[0-9]{1,3} [0-9]{10}$/.test(phone);
+    }
+
+    isValidEmail(email) {
+        return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    }
 }
-let add=new AddressClass("deepak","Singh","House no 45","Bhopal","Madhya Pradesh","462046","9691876648","singh017deepak@gmail.com");
-console.log(add);
+
+// Example Usage
+try {
+    let contact1 = new Contact("John", "Doe", "Street 123", "New York", "NY", "10001", "1 9876543210", "john.doe@example.com");
+    console.log("Valid Contact:", contact1);
+} catch (error) {
+    console.error(error.message);
+}
+
+try {
+    let contact2 = new Contact("jo", "smith", "St", "LA", "CA", "123", "98765", "invalid-email");
+} catch (error) {
+    console.error(error.message);
+}
